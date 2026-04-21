c2fa2d200839d5469279ad18cd197b3ba5234b99
Alisson <alissonxspx98@gmail.com>
2026-04-21 13:05:39 -0300

    Primeiro commit Atuomação de consumo diário

---
+++ AutomaçãoDeConsumoDiário.py
@@ -0,0 +1,463 @@
+import time
+import pyautogui as py
+
+py.PAUSE = 3
+
+#abri smp
+py.hotkey("alt", "tab")
+#ir no + do bradesco
+py.click(x=30, y=153)
+#clicar no Amarelo
+py.doubleClick(x=145, y=200)
+#clicar nos dados
+py.click(x=971, y=117)
+
+#esperar 5 segundos
+time.sleep(5)
+
+#copiar dados
+py.click(x=466, y=154)
+
+#abrir tabela de consumo
+py.press("win")
+py.write("mapeamento")
+py.press('enter')
+
+#clicar no icode do Amarelo e  olar
+py.click(x=248, y=830)
+py.hotkey("ctrl", "v")
+
+
+  #Copiar dados do SMP
+#volta pro smp
+py.hotkey("alt", "tab")
+
+#abrir Azul
+py.doubleClick(x=135, y=218)
+
+#clicar nos dados
+py.click(x=971, y=117)
+
+#esperar 5 segundos
+time.sleep(5)
+#copiar dados
+py.click(x=466, y=154)
+
+#foltar pra tabela
+py.hotkey("alt", "tab")
+
+#clicar no icode do Amarelo e colar
+py.click(x=296, y=830)
+py.hotkey("ctrl", "v")
+
+
+   #Copiar dados do SMP CINZA
+#volta pro smp
+py.hotkey("alt", "tab")
+
+#abrir Cinza
+py.doubleClick(x=123, y=236)
+
+#clicar nos dados
+py.click(x=971, y=117)
+
+#esperar 5 segundos
+time.sleep(8)
+#copiar dados
+py.click(x=466, y=154)
+
+#foltar pra tabela
+py.hotkey("alt", "tab")
+
+#clicar no icode do Cinza e colar
+py.click(x=326, y=830)
+py.hotkey("ctrl", "v")
+
+
+    #Copiar dados do SMP MARROM
+#volta pro smp
+py.hotkey("alt", "tab")
+
+#abrir Marron
+py.doubleClick(x=149, y=254)
+
+#clicar nos dados
+py.click(x=971, y=117)
+
+#esperar 5 segundos
+time.sleep(5)
+#copiar dados
+py.click(x=466, y=154)
+
+#foltar pra tabela
+py.hotkey("alt", "tab")
+
+#clicar no icode do Marron e colar
+py.click(x=418, y=822)
+py.hotkey("ctrl", "v")
+
+
+
+#Copiar dados do SMP
+#volta pro smp
+py.hotkey("alt", "tab")
+
+#abrir
+py.doubleClick(x=132, y=272)
+
+#clicar nos dados
+py.click(x=971, y=117)
+
+#esperar 5 segundos
+time.sleep(5)
+#copiar dados
+py.click(x=466, y=154)
+
+#foltar pra tabela
+py.hotkey("alt", "tab")
+
+#clicar no icode do  e colar
+py.click(x=481, y=826)
+py.hotkey("ctrl", "v")
+
+
+
+#Copiar dados do SMP UNIBRAD
+#volta pro smp
+py.hotkey("alt", "tab")
+
+#abrir UNIBRAD
+py.doubleClick(x=128, y=290)
+
+#clicar nos dados
+py.click(x=971, y=117)
+
+#esperar 5 segundos
+time.sleep(5)
+#copiar dados
+py.click(x=466, y=154)
+
+#foltar pra tabela
+py.hotkey("alt", "tab")
+
+#clicar no icode do UNIBRAD e colar
+py.click(x=547, y=828)
+py.hotkey("ctrl", "v")
+
+
+#Copiar dados do SMP Rubi
+#volta pro smp
+py.hotkey("alt", "tab")
+
+#abrir Rubi
+py.doubleClick(x=116, y=310)
+
+#clicar nos dados
+py.click(x=971, y=117)
+
+#esperar 5 segundos
+time.sleep(5)
+#copiar dados
+py.click(x=466, y=154)
+
+#foltar pra tabela
+py.hotkey("alt", "tab")
+
+#clicar no icode do Rubi e colar
+py.click(x=594, y=819)
+py.hotkey("ctrl", "v")
+
+
+#Copiar dados do SMP Vermelho
+#volta pro smp
+py.hotkey("alt", "tab")
+
+#abrir Vermelho
+py.doubleClick(x=120, y=327)
+
+#clicar nos dados
+py.click(x=971, y=117)
+
+#esperar 5 segundos
+time.sleep(5)
+#copiar dados
+py.click(x=466, y=154)
+
+#foltar pra tabela
+py.hotkey("alt", "tab")
+
+#clicar no icode do Vermelho e colar
+py.click(x=652, y=830)
+py.hotkey("ctrl", "v")
+
+
+#Copiar dados do SMP Reservatorio
+#volta pro smp
+py.hotkey("alt", "tab")
+
+#abrir Reservatorio
+py.doubleClick(x=124, y=347)
+
+#clicar nos dados
+py.click(x=971, y=117)
+
+#esperar 5 segundos
+time.sleep(5)
+#copiar dados
+py.click(x=466, y=154)
+
+#foltar pra tabela
+py.hotkey("alt", "tab")
+
+#clicar no icode do Reservatorio e colar
+py.click(x=766, y=821)
+py.hotkey("ctrl", "v")
+
+
+#Copiar dados do SMP Estudio
+#volta pro smp
+py.hotkey("alt", "tab")
+
+#abrir Estudio
+py.doubleClick(x=125, y=371)
+
+#clicar nos dados
+py.click(x=971, y=117)
+
+#esperar 5 segundos
+time.sleep(5)
+#copiar dados
+py.click(x=466, y=154)
+
+#foltar pra tabela
+py.hotkey("alt", "tab")
+
+#clicar no icode do Estudio e colar
+py.click(x=856, y=834)
+py.hotkey("ctrl", "v")
+
+
+
+#Copiar dados do SMP Paulista
+#volta pro smp
+py.hotkey("alt", "tab")
+
+#abrir Paulista
+py.doubleClick(x=117, y=385)
+
+#clicar nos dados
+py.click(x=971, y=117)
+
+#esperar 5 segundos
+time.sleep(10)
+#copiar dados
+py.click(x=466, y=154)
+
+#foltar pra tabela
+py.hotkey("alt", "tab")
+
+#clicar no icode do Paulista e colar
+py.click(x=850, y=830)
+py.hotkey("ctrl", "v")
+
+
+#Copiar dados do SMP Itapeva
+#volta pro smp
+py.hotkey("alt", "tab")
+
+#abrir Itapeva
+py.doubleClick(x=113, y=402)
+
+#clicar nos dados
+py.click(x=971, y=117)
+
+#esperar 5 segundos
+time.sleep(10)
+#copiar dados
+py.click(x=466, y=154)
+
+#foltar pra tabela
+py.hotkey("alt", "tab")
+
+#clicar no icode do  e colar
+py.click(x=910, y=827)
+py.hotkey("ctrl", "v")
+
+
+#Copiar dados do SMP Campinas
+#volta pro smp
+py.hotkey("alt", "tab")
+
+#abrir Campinas
+py.doubleClick(x=110, y=424)
+
+#clicar nos dados
+py.click(x=971, y=117)
+
+#esperar 5 segundos
+time.sleep(10)
+#copiar dados
+py.click(x=466, y=154)
+
+#foltar pra tabela
+py.hotkey("alt", "tab")
+
+#clicar no icode do Campinas e colar
+py.click(x=910, y=827)
+py.hotkey("ctrl", "v")
+
+
+
+#Copiar dados do SMP Palácio Avenida
+#volta pro smp
+py.hotkey("alt", "tab")
+
+#abrir Palácio Avenida
+py.doubleClick(x=115, y=445)
+
+#clicar nos dados
+py.click(x=971, y=117)
+
+#esperar 5 segundos
+time.sleep(8)
+#copiar dados
+py.click(x=466, y=154)
+
+#voltar pra tabela
+py.hotkey("alt", "tab")
+
+#clicar no icode do Palácio Avenida e colar
+py.click(x=901, y=829)
+py.hotkey("ctrl", "v")
+
+
+#Copiar dados do SMP Xaxim
+#volta pro smp
+py.hotkey("alt", "tab")
+
+#abrir Xaxim
+py.doubleClick(x=114, y=464)
+#clicar nos dados
+py.click(x=971, y=117)
+
+#esperar 5 segundos
+time.sleep(5)
+#copiar dados
+py.click(x=466, y=154)
+
+#voltar pra tabela
+py.hotkey("alt", "tab")
+
+#clicar no icode do Xaxim e colar
+py.click(x=901, y=829)
+py.hotkey("ctrl", "v")
+
+
+
+#Copiar dados do SMP Cinelandia
+#volta pro smp
+py.hotkey("alt", "tab")
+
+#abrir Cinelandia
+py.doubleClick(x=116, y=481)
+
+#clicar nos dados
+py.click(x=971, y=117)
+
+#esperar 5 segundos
+time.sleep(5)
+#copiar dados
+py.click(x=466, y=154)
+
+#foltar pra tabela
+py.hotkey("alt", "tab")
+
+#clicar no icode do Cinelandia e colar
+py.click(x=901, y=829)
+py.hotkey("ctrl", "v")
+
+#FINAL DA COLETA DE DADOS
+
+
+#TRATAMENTO DE DADOS
+#ir pro começo
+py.mouseDown(x=29, y=823)
+
+#copiar valores
+time.sleep(3)
+py.mouseUp(x=29, y=823)
+py.click(x=147, y=825)
+py.click(x=720, y=383)
+
+#selevionar valores
+time.sleep(8)
+py.hotkey("ctrl", "c")
+py.press('win')
+py.write('Documentos: consumo diario.xlsx')
+py.press('enter')
+time.sleep(8)
+
+
+
+#colar dados
+py.click(x=700, y=387)#selecionar cedula
+py.click(x=44, y=112) #colar
+py.click(x=81, y=268) #somente valores
+py.press('enter')
+
+#troca de ponto por virgula
+
+py.click(x=1143, y=213)
+py.click(x=1342, y=129)
+py.click(x=1381, y=182)
+py.write('.')
+py.press('tab')
+py.write(',')
+py.press('tab')
+py.press('tab')
+py.press('enter')
+time.sleep(3)
+py.press('enter')#conclui
+
+#abri outlook
+py.press('win')
+py.write('outlook')
+time.sleep(5)
+py.press('enter')
+py.click(x=1381, y=182)
+#novo email
+py.click(x=84, y=96)
+#destinartário Vinicius Dias
+
+
+py.write('vinicius.Dias@jll.com')
+py.press('enter')
+py.press('tab')
+py.write('Eduardo.Kuwabara@jci.com')
+py.press('enter')
+py.write('BTS-operacaobradesco@jci.com')
+py.press('enter')
+py.write('dennis.ferreiracarvalho@jci.com')
+py.press('enter')
+
+#assunto
+py.press('tab')
+py.write('CONSUMO MULTIMEDIDORES - NCD e FNCD - DD/MM')
+
+#anexo
+py.click(x=892, y=87)
+py.press('tab')
+py.press('enter')
+
+py.press('tab')
+py.press('enter')
+
+py.press('tab')
+py.press('tab')
+
+#escrever msg
+
+py.write('Prezado, bom dia')
+py.press('enter')
+py.press('enter')
+py.write('Segue anexo a leitura de consumo dos multimedidores do dia  extraídos a 00:00')
